58ac8088c06d91af0d3fceaf408e058e0b183f87
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-06 17:44:34 +0000

    Extract method

--- test_RSR.lua
+++ test_RSR.lua
@@ -4,37 +4,31 @@ dofile("RSR.lua")
 
 TestMarkRemoved = {}
 
+function removeMarkWithText(text)
+    event = {
+        id = world.event.S_EVENT_MARK_REMOVED,
+        text = text,
+        pos = { x = 1, y = 2, z = 3 }
+    }
+    markRemoved(event)
+end
+
 function TestMarkRemoved:setUp()
     dcsStub.recordedCalls = {}
 end
 
 function TestMarkRemoved:testSpawnsCrate()
-    event = {
-        id = world.event.S_EVENT_MARK_REMOVED,
-        text = "-crate 100",
-        pos = { x = 1, y = 2, z = 3 }
-    }
-    markRemoved(event)
+    removeMarkWithText("-crate 100")
     dcsStub.assertOneCallTo("coalition.addStaticObject")
 end
 
 function TestMarkRemoved:testInvalidWeightDisplaysMessage()
-    event = {
-        id = world.event.S_EVENT_MARK_REMOVED,
-        text = "-crate 123",
-        pos = { x = 1, y = 2, z = 3 }
-    }
-    markRemoved(event)
+    removeMarkWithText("-crate 123")
     dcsStub.assertOneCallTo("trigger.action.outText")
 end
 
 function TestMarkRemoved:testOtherTextDoesNothing()
-    event = {
-        id = world.event.S_EVENT_MARK_REMOVED,
-        text = "this is my message",
-        pos = { x = 1, y = 2, z = 3 }
-    }
-    markRemoved(event)
+    removeMarkWithText("tis is my message")
     dcsStub.assertNoCalls()
 end
 
