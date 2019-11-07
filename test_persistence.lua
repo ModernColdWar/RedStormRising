8f72bd85a0a5e846f9a3e77f88567c89e4293103
David Dunwoody <david.dunwoody@gmail.com>
2019-11-07 17:12:00 +0000

    Add JSON.lua from http://regex.info/code/JSON.lua

---
+++ test_persistence.lua
@@ -0,0 +1,35 @@
+local lu = require("luaunit")
+dofile("RSR.lua")
+
+TestMarkEvents = {}
+
+function removeMarkWithText(text)
+    event = {
+        id = world.event.S_EVENT_MARK_REMOVED,
+        text = text,
+        pos = { x = 1, y = 2, z = 3 }
+    }
+    markRemoved(event)
+end
+
+function TestMarkEvents:setUp()
+    dcsStub.reset()
+end
+
+function TestMarkEvents:testSpawnsCrate()
+    removeMarkWithText("-crate 100")
+    dcsStub.assertOneCallTo("coalition.addStaticObject")
+end
+
+function TestMarkEvents:testInvalidWeightDisplaysMessage()
+    removeMarkWithText("-crate 123")
+    dcsStub.assertOneCallTo("trigger.action.outText")
+end
+
+function TestMarkEvents:testOtherTextDoesNothing()
+    removeMarkWithText("tis is my message")
+    dcsStub.assertNoCalls()
+end
+
+local runner = lu.LuaUnit.new()
+os.exit(runner:runSuite())
