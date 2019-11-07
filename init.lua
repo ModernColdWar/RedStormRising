0b59647ecab7855ac01a54bd4d8e3dfb6ad6eee0
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-07 21:32:33 +0000

    First version of saving and respawning CTLD units

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
+local writeInterval = 10
 
 if stateFileExists() then
     log:info("State file exists - will set up from saved state")
-    local saved_state = readState()
+    readState()
+    spawnFromState()
 else
     log:info("No state file exists - setting up from scratch")
 end
 
-mist.scheduleFunction(writeState, {rsrState}, timer.getTime() + 30, 30)
+mist.scheduleFunction(writeState, {true}, timer.getTime() + writeInterval, writeInterval)
