23d5f1640122f7f1b4a55bf98aba47c4196947bd
David Dunwoody <david.dunwoody@gmail.com>
2019-11-07 23:41:21 +0000

    Save CTLD id sequences; allow specifying state file name

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ if stateFileExists() then
     log:info("State file exists - will set up from saved state")
     readState()
     spawnFromState()
+    ctld.nextGroupId = rsrState.ctld.nextGroupId
+    ctld.nextUnitId = rsrState.ctld.nextUnitId
 else
     log:info("No state file exists - setting up from scratch")
 end
