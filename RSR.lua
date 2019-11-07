10589cd40202ee9e1136330c38994737c4a32d4f
David Dunwoody <david.dunwoody@gmail.com>
2019-11-07 16:36:47 +0000

    Extract markEvents.lua

--- RSR.lua
+++ RSR.lua
@@ -1,50 +1,35 @@
 --- Red Storm Rising DCS mission LUA code
 
 --- Figure out whether we're running inside DCS or not
--- currently done by setting environment variable manually when running outside
-function runningInDcs()
-    return os.getenv("STUB_DCS") == nil
+-- currently done by setting environment variable manually when running outside or loading dcs_stub.lua
+local function runningInDcs()
+    return os.getenv("STUB_DCS") == nil and dcsStub == nil
 end
 
 if not runningInDcs() then
     dofile("dcs_stub.lua")
 end
 
+env.info("RSR starting")
+
 --- Loads file from inside DCS saved games folder if running in DCS or current dir if not
-function dofileWrapper(filename)
+local function dofileWrapper(filename)
+    env.info("Loading " .. filename)
     if runningInDcs() then
         dofile(lfs.writedir() .. [[Scripts\RSR\]] .. filename)
     else
         dofile(filename)
     end
+    env.info(filename .. " loaded")
 end
 
-env.info("RSR starting")
-
-env.info("Loading MIST")
 dofileWrapper("mist_4_3_74.lua")
-env.info("MIST loaded")
-
-log = mist.Logger:new("RSR", "info")
-
-log:info("Loading CTLD")
 dofileWrapper("CTLD.lua")
-log:info("CTLD loaded")
 
 ctld.slingLoad = true
 
-function markRemoved(event)
-    if event.id == world.event.S_EVENT_MARK_REMOVED and event.text ~= nil then
-        local text = event.text:lower()
-        local side = event.coalition == coalition.side.RED and "red" or "blue"
-        if text:find("-crate") then
-            local weight = tonumber(string.sub(text, 8))
-            log:info("Spawing $1 crate with weight $2 at $3", side, weight, event.pos)
-            ctld.spawnCrateAtPoint(side, weight, event.pos)
-        end
-    end
-end
+log = mist.Logger:new("RSR", "info")
 
-mist.addEventHandler(markRemoved)
+dofileWrapper("markEvents.lua")
 
-log:info("RSR ready")
+env.info("RSR ready")
---
+++ markEvents.lua
@@ -0,0 +1,13 @@
+function markRemoved(event)
+    if event.id == world.event.S_EVENT_MARK_REMOVED and event.text ~= nil then
+        local text = event.text:lower()
+        local side = event.coalition == coalition.side.RED and "red" or "blue"
+        if text:find("-crate") then
+            local weight = tonumber(string.sub(text, 8))
+            log:info("Spawing $1 crate with weight $2 at $3", side, weight, event.pos)
+            ctld.spawnCrateAtPoint(side, weight, event.pos)
+        end
+    end
+end
+
+mist.addEventHandler(markRemoved)
