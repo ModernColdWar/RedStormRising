973560fe35e98176057b7a67cd232521f66d6666
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-07 23:41:21 +0000

    Save CTLD id sequences; allow specifying state file name

--- persistence.lua
+++ persistence.lua
@@ -1,6 +1,7 @@
 rsrState = {
     persistentGroupData = {},
     ctldUnpackedGroupNames = {},
+    ctld = {},
 }
 
 function updateState()
@@ -9,24 +10,28 @@ function updateState()
         log:info("Getting data for unpacked CTLD unit: $1", groupName)
         table.insert(rsrState.persistentGroupData, mist.getGroupData(groupName))
     end
+    rsrState.ctld.nextGroupId = ctld.nextGroupId
+    rsrState.ctld.nextUnitId = ctld.nextUnitId
 end
 
 local stateFile = getFilePath("rsrState.json")
 
-function writeState(updateFirst)
+function writeState(updateFirst, filename)
     if updateFirst then
         updateState()
     end
-    log:info("Writing state to disk at $1", stateFile)
-    local f = io.open(stateFile, "w")
+    filename = filename or stateFile
+    log:info("Writing state to disk at $1", filename)
+    local f = io.open(filename, "w")
     f:write(JSON:encode(rsrState))
     f:close()
     log:info("Finished writing state")
 end
 
-function readState()
-    log:info("Reading state from disk at $1", stateFile)
-    local f = io.open(stateFile, "r")
+function readState(filename)
+    filename = filename or stateFile
+    log:info("Reading state from disk at $1", filename)
+    local f = io.open(filename, "r")
     local stateJson = f:read("*all")
     f:close()
     rsrState = JSON:decode(stateJson)
