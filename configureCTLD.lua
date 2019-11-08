19cbb48ecd39b08670c2815b937f20ea91055486
David Dunwoody <david.dunwoody@gmail.com>
2019-11-08 00:19:55 +0000

    Reduce crate spawn timer to 1s

--- configureCTLD.lua
+++ configureCTLD.lua
@@ -5,7 +5,7 @@ ctld.maximumSearchDistance = 0 -- max distance for troops to search for enemy
 ctld.minimumDeployDistance = 600 -- minimum distance from a friendly pickup zone where you can deploy a crate
 ctld.cratesRequiredForFOB = 1 -- The amount of crates required to build a FOB. Once built, helis can spawn crates at this outpost to be carried and deployed in another area.
 ctld.buildTimeFOB = 30 --time in seconds for the FOB to be built
-ctld.crateWaitTime = 5 -- time in seconds to wait before you can spawn another crate
+ctld.crateWaitTime = 1 -- time in seconds to wait before you can spawn another crate
 ctld.forceCrateToBeMoved = false -- a crate must be picked up at least once and moved before it can be unpacked. Helps to reduce crate spam. Only works if ctld.slingLoad = false -Swallow you might want to change this to True
 
 ctld.unitActions = {
