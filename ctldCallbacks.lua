dbaa0d6c61062ec112aa4098870ab0ae4a943813
David Dunwoody <david.dunwoody@gmail.com>
2019-11-08 11:58:06 +0000

    Reduce global state; don't persist groupId/unitId so MIST reassigns on spawn

--- ctldCallbacks.lua
+++ ctldCallbacks.lua
@@ -1,11 +1,11 @@
-local function unpack(_args)
-    log:info('Unpack: $1', _args)
+local function unpack(state, _args)
+    log:info('Unpacked: $1', _args)
     local groupName = _args.spawnedGroup:getName()
-    table.insert(rsrState.ctldUnpackedGroupNames, groupName)
+    table.insert(state.ctldUnpackedGroupNames, groupName)
 end
 
 ctld.addCallback(function(_args)
     if _args.action and _args.action == "unpack" then
-        unpack(_args)
+        unpack(rsr.state, _args)
     end
 end)
