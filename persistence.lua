5a9934fedcdc3506bc17bd131c2201a3b8c1a375
David Dunwoody <david.dunwoody@gmail.com>
2019-11-11 17:32:59 +0000

    Basic fixes once running in DCS

--- persistence.lua
+++ persistence.lua
@@ -1,6 +1,6 @@
 --- Saving/loading/updating code for managing "live" units and persisting them across server restarts
 local utils = require("utils")
-
+local JSON = require("JSON")
 
 -- recently spawned units (from player unpacking via CTLD or via code)
 local spawnQueue = {}
@@ -17,9 +17,12 @@ local state = {
 local function readStateFromDisk(filename)
     log:info("Reading state from disk at $1", filename)
     local f = io.open(filename, "r")
-    local stateJson = f:read("*all")
+    local json = f:read("*all")
     f:close()
-    local _state = JSON:decode(stateJson)
+    if rsr.devMode then
+        log:info("JSON read from disk:\n$1", json)
+    end
+    local _state = JSON:decode(json)
     log:info("Finished reading state from disk at $1", filename)
     return _state
 end
@@ -37,11 +40,15 @@ function removeGroupAndUnitIds(persistentGroupData)
 end
 
 local function writeStateToDisk(_state, filename)
-    local copiedState = mist.utils.deepCopy(_state)
-    removeGroupAndUnitIds(copiedState.persistentGroupData)
+    local stateToWrite = mist.utils.deepCopy(_state)
+    removeGroupAndUnitIds(stateToWrite.persistentGroupData)
     log:info("Writing state to disk at $1", filename)
+    local json = JSON:encode_pretty(stateToWrite)
+    if rsr.devMode then
+        log:info("JSON being written to disk:\n$1", json)
+    end
     local f = io.open(filename, "w")
-    f:write(JSON:encode(stateToWrite))
+    f:write(json)
     f:close()
     log:info("Finished writing state to $1", filename)
 end
@@ -56,9 +63,10 @@ local function handleSpawnQueue()
         if groupData ~= nil then
             log:info("Successfully got group data for $1", groupName)
             table.insert(state.persistentGroupData, groupData)
+            log:info("Removing $1 from spawn queue", groupName)
             table.remove(spawnQueue, i)
         else
-            log:warn("Unable to get group data for $1", groupName)
+            log:warn("Unable to get group data for $1; leaving in spawn queue", groupName)
         end
     end
     log:info("Spawn queue handling complete")
@@ -67,7 +75,7 @@ end
 local function updateGroupData(persistentGroupData)
     log:info("Updating persistent group data")
     for i = #persistentGroupData, 1, -1 do
-        local groupName = persistentGroupData["name"]
+        local groupName = persistentGroupData[i]["name"]
         log:info("Getting data for group $1", groupName)
         local groupData = mist.getCurrentGroupData(groupName)
         if groupData == nil then
@@ -75,7 +83,7 @@ local function updateGroupData(persistentGroupData)
             table.remove(persistentGroupData, i)
         else
             log:info("Updating group data for $1 to $2", groupName, groupData)
-            table[i] = groupData
+            persistentGroupData[i] = groupData
         end
     end
     log:info("Persistent group data update complete")
@@ -93,12 +101,21 @@ local function persistState()
     if utils.fileExists(rsr.stateFileName) then
         utils.createBackup(rsr.stateFileName)
     end
-    writeStateToDisk(_state, rsr.stateFileName)
+    writeStateToDisk(state, rsr.stateFileName)
+end
+
+local function pushSpawnQueue(groupName)
+    log:info("Adding $1 to spawn queue", groupName)
+    table.insert(spawnQueue, groupName)
 end
 
 local function restoreFromState(_state)
     --- Note that we don't directly update the state variable from here, this is done in handleSpawnQueue later
     log:info("Restoring from state")
+    if _state == nil then
+        log:warn("State loaded from disk is nil - setting up from scratch")
+        return
+    end
     ctld.nextGroupId = _state.ctld.nextGroupId
     ctld.nextUnitId = _state.ctld.nextUnitId
 
@@ -106,7 +123,7 @@ local function restoreFromState(_state)
         local groupName = groupData.groupName
         log:info("Spawning $1 from saved state $2", groupName, groupData)
         mist.dynAdd(groupData)
-        table.insert(spawnQueue, groupData.groupName)
+        pushSpawnQueue(groupName)
     end
     log:info("Restored from state")
 end
@@ -124,7 +141,7 @@ if utils.runningInDcs() then
         if _args.action and _args.action == "unpack" then
             log:info('Unpacked: $1', _args)
             local groupName = _args.spawnedGroup:getName()
-            table.insert(spawnQueue, groupName)
+            pushSpawnQueue(groupName)
         end
     end)
 
