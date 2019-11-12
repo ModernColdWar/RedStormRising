81f6fbe446d6ba1488ed4891c3dd788b894476b1
David Dunwoody <david.dunwoody@gmail.com>
2019-11-12 15:32:10 +0000

    Suffix crate quantity with "c"

--- MCW.lua
+++ MCW.lua
@@ -5110,7 +5110,7 @@ function ctld.addF10MenuOptions()
 
                                                 --add in the number of crates required to build something
                                                 if _crate.cratesRequired ~= nil and _crate.cratesRequired > 1 then
-                                                    _crateRadioMsg = _crateRadioMsg .. " (" .. _crate.cratesRequired .. ")"
+                                                    _crateRadioMsg = _crateRadioMsg .. " (" .. _crate.cratesRequired .. "c)"
                                                 end
 
                                                 missionCommands.addCommandForGroup(_groupId, _crateRadioMsg, _cratePath, ctld.spawnCrate, { _unitName, _crate.weight, _crate.internal })
