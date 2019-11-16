b4c48ad738b32e2621c92b71aec24e2df83da083
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-16 16:42:29 +0000

    Implement matchesBaseName function

--- slotBlocker.lua
+++ slotBlocker.lua
@@ -18,6 +18,49 @@ function M.getBaseAndSideNames(groupName)
     return baseName, sideName:lower()
 end
 
+local function startswith(string, prefix)
+    if string:sub(1, #prefix) == prefix then
+        return true
+    end
+    return false
+end
+
+local function split(string, sep)
+    local fields = {}
+    local pattern = string.format("([^%s]+)", sep)
+    string:gsub(pattern, function(c)
+        fields[#fields + 1] = c
+    end)
+    return fields
+end
+
+-- Matches a base name against a groupPrefix
+-- is fairly generous in that you only need the distinguishing prefix on the group
+-- with each word being treated independently
+function M.matchesBaseName(baseName, groupPrefix)
+    if groupPrefix == nil then
+        return false
+    end
+    if startswith(baseName, groupPrefix) then
+        return true
+    end
+
+    local baseNameParts = split(baseName, "-")
+    local groupPrefixParts = split(groupPrefix, "-")
+
+    if #baseNameParts < #groupPrefixParts then
+        return false
+    end
+    for i = 1, #groupPrefixParts do
+        local baseNamePart = baseNameParts[i]
+        local groupPrefixPart = groupPrefixParts[i]
+        if startswith(baseNamePart, groupPrefixPart) == false then
+            return false
+        end
+    end
+    return true
+end
+
 local function disableSlot(groupName)
     log:info("Disabling $1", groupName)
     trigger.action.setUserFlag(groupName, 1)
--- tests/test_slotBlocker.lua
+++ tests/test_slotBlocker.lua
@@ -14,6 +14,22 @@ function TestSlotBlocker:testGetBaseAndSideNames()
     lu.assertEquals(sideName, "blue")
 end
 
+function TestSlotBlocker:testMatchesBaseName()
+    lu.assertIsFalse(slotBlocker.matchesBaseName("foo", nil))
+    lu.assertIsFalse(slotBlocker.matchesBaseName("foo", "bar"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Senaki", "Senaki"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Senaki-Kolkhi", "Senaki"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Senaki", "Sen"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Senaki", "S"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "Krasnodar-Pashkovsky"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "Krasnodar-P"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "Kras-P"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "K-P"))
+    lu.assertIsTrue(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "K-Pash"))
+    lu.assertIsFalse(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "Kras-C"))
+    lu.assertIsFalse(slotBlocker.matchesBaseName("Krasnodar-Pashkovsky", "Kras-P-D"))
+end
+
 local runner = lu.LuaUnit.new()
 os.exit(runner:runSuite())
 
