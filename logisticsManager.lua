73680e9b6031c2ebe3f22d82cb8177d207c2415f
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-18 17:59:52 +0000

    Spawn using ctld

--- logisticsManager.lua
+++ logisticsManager.lua
@@ -6,14 +6,13 @@ local M = {}
 local log = mist.Logger:new("LogisticsManager", "info")
 
 function M.spawnLogisticsBuildingForBase(baseName, sideName)
-    log:info("Spawning logistics building for $1 as owned by $2", baseName, sideName)
+    log:info("Spawning FOB building for $1 as owned by $2", baseName, sideName)
     local country = sideName == "red" and country.id.RUSSIA or country.id.USA
-    local spawnLogistics = SPAWNSTATIC:NewFromStatic("Logistics Template", country)
     local logisticsName = baseName .. " Logistics"
     local heading = ctld.logisticUnitsHeadings[logisticsName]
-    local zone = ZONE:New(logisticsName)
-    log:info("Spawning logistics building $1 with heading $2 for $3", logisticsName, heading, sideName)
-    spawnLogistics:SpawnFromZone(zone, heading, logisticsName)
+    local point = ZONE:New(logisticsName):GetPointVec2()
+    ctld.spawnFOB(country, nil, point, logisticsName, heading)
+    log:info("Spawned $1 $2 FOB", sideName, logisticsName)
 end
 
 return M
