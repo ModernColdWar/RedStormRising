b16cc420ba2b615d2ad73d9bc46e281160649627
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-19 21:17:02 +0000

    Handle mission logistics zones

--- logisticsManager.lua
+++ logisticsManager.lua
@@ -9,6 +9,10 @@ function M.spawnLogisticsBuildingForBase(baseName, sideName)
     local country = sideName == "red" and country.id.RUSSIA or country.id.USA
     local logisticsName = baseName .. " Logistics"
     local heading = ctld.logisticUnitsHeadings[logisticsName]
+    if trigger.misc.getZone(logisticsName) == nil then
+        log:warn("No logistics zone called '$1' found; no logistics building will spawn", logisticsName)
+        return
+    end
     local point = ZONE:New(logisticsName):GetPointVec2()
     ctld.spawnFOB(country, nil, point, logisticsName, heading)
     log:info("Spawned $1 $2 FOB", sideName, logisticsName)
