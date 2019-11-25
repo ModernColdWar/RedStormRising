5e63f25bd9b5d7e6a4db460a8759cba4b004a858
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-11-25 15:34:56 +0000

    Log takeoff and landing events

--- loggingEventHandler.lua
+++ loggingEventHandler.lua
@@ -1,20 +1,27 @@
 local M = {}
 
-local log = mist.Logger:new("HitEventHandler", "info")
+M.eventHandler = EVENTHANDLER:New()
+
+function M.eventHandler:logEvent(event)
+    self:I({ event = event })
+end
+
+function M.eventHandler:onHit(event)
+    self:logEvent(event)
+    if event.IniPlayerName == nil or event.TgtPlayerName == nil then
+        return
+    end
+    if event.WeaponTypeName == nil then
+        log:info("$1 hit $2", event.IniPlayerName, event.TgtPlayerName)
+    else
+        log:info("$1 hit $2 with $3", event.IniPlayerName, event.TgtPlayerName, event.WeaponTypeName)
+    end
+end
 
 function M.register()
-    M.eventHandler = EVENTHANDLER:New():HandleEvent(EVENTS.Hit)
-    function M.eventHandler:OnEventHit(event)
-        self:I({ event = event })
-        if event.IniPlayerName == nil or event.TgtPlayerName == nil then
-            return
-        end
-        if event.WeaponTypeName == nil then
-            log:info("$1 hit $2", event.IniPlayerName, event.TgtPlayerName)
-        else
-            log:info("$1 hit $2 with $3", event.IniPlayerName, event.TgtPlayerName, event.WeaponTypeName)
-        end
-    end
+    M.eventHandler:HandleEvent(EVENTS.Takeoff, M.eventHandler.logEvent)
+    M.eventHandler:HandleEvent(EVENTS.Land, M.eventHandler.logEvent)
+    M.eventHandler:HandleEvent(EVENTS.Hit, M.eventHandler.onHit)
 end
 
 return M
