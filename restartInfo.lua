3c3c16e301e20745b299e946569d7aa6a2da15b9
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-12-09 14:59:54 +0000

    Add helper functions for restart info

---
+++ restartInfo.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+
+function M.getNextRestartHour(currentHour, restartHours)
+    for _, restartHour in ipairs(restartHours) do
+        if restartHour > currentHour then
+            return restartHour
+        end
+    end
+    return restartHours[1]
+end
+
+function M.getSecondsUntilRestart(now, restartHours)
+    local restartHour = M.getNextRestartHour(now.hour, restartHours)
+    local restartDate = os.time({year=now.year, month=now.month, day=now.day, hour=restartHour})
+    local seconds = restartDate - os.time(now)
+    if seconds <= 0 then
+        seconds = seconds + 86400
+    end
+    return seconds
+end
+
+function M.getSecondsAsString(seconds)
+    local remSeconds = seconds
+    local hours = math.floor(seconds / 3600)
+    remSeconds = remSeconds - (hours * 3600)
+    local minutes = math.floor(remSeconds / 60)
+    remSeconds = seconds - (minutes * 60) - (hours * 3600)
+    return string.format("%02d:%02d:%02d", hours, minutes, remSeconds)
+end
+
+return M
---
+++ tests/test_restartInfo.lua
@@ -0,0 +1,32 @@
+local lu = require("tests.luaunit")
+local restartInfo = require("restartInfo")
+
+TestRestartInfo = {}
+
+function TestRestartInfo:testGetNextRestartHour()
+    lu.assertEquals(restartInfo.getNextRestartHour(0, { 1 }), 1)
+    lu.assertEquals(restartInfo.getNextRestartHour(1, { 1 }), 1)
+    lu.assertEquals(restartInfo.getNextRestartHour(2, { 1 }), 1)
+    lu.assertEquals(restartInfo.getNextRestartHour(2, { 1, 5, 9 }), 5)
+    lu.assertEquals(restartInfo.getNextRestartHour(5, { 1, 5, 9 }), 9)
+    lu.assertEquals(restartInfo.getNextRestartHour(9, { 1, 5, 9 }), 1)
+end
+
+function TestRestartInfo:testGetSecondsUntilRestart()
+    lu.assertEquals(restartInfo.getSecondsUntilRestart({ year = 2019, month = 9, day = 12, hour = 12, min = 59, sec = 15 }, { 13 }), 45)
+    lu.assertEquals(restartInfo.getSecondsUntilRestart({ year = 2019, month = 9, day = 12, hour = 23, min = 59, sec = 0 }, { 1 }), 3660)
+    lu.assertEquals(restartInfo.getSecondsUntilRestart({ year = 2019, month = 9, day = 12, hour = 12, min = 0, sec = 0 }, { 12 }), 86400)
+end
+
+function TestRestartInfo:testGetSecondsAsString()
+    lu.assertEquals(restartInfo.getSecondsAsString(1), "00:00:01")
+    lu.assertEquals(restartInfo.getSecondsAsString(60), "00:01:00")
+    lu.assertEquals(restartInfo.getSecondsAsString(150), "00:02:30")
+    lu.assertEquals(restartInfo.getSecondsAsString(900), "00:15:00")
+    lu.assertEquals(restartInfo.getSecondsAsString(3600), "01:00:00")
+    lu.assertEquals(restartInfo.getSecondsAsString(12345), "03:25:45")
+end
+
+local runner = lu.LuaUnit.new()
+os.exit(runner:runSuite())
+
