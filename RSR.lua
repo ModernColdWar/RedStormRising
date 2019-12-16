068700e340a0662652b994856c125cdaa5876e49
Winston2108 <david.dunwoody+winston2108@gmail.com>
2019-12-16 13:33:06 +0000

    Make CTLD even quicker in dev mode

--- RSR.lua
+++ RSR.lua
@@ -19,6 +19,8 @@ local rsrConfig = require("RSR_config")
 if rsrConfig.devMode then
     log:warn("Running in developer mode - should not be used for 'real' servers")
     ctld.debug = true
+    ctld.buildTimeFOB = 5
+    ctld.crateWaitTime = 1
 end
 
 local persistence = require("persistence")
