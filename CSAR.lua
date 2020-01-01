3e9090515d0c305a7dd275059bec4d3326c717d4
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-01-01 16:17:56 +0000

    Live increased to 10 on server

--- CSAR.lua
+++ CSAR.lua
@@ -1989,7 +1989,7 @@ csar.csarMode = 3
 --      2 - Disable Aircraft for Pilot when he's shot down -- timeout to reenable pilot for aircraft
 --      3 - Pilot Life Limit - No Aircraft Disabling -- timeout to reset lives?
 
-csar.maxLives = 6 -- Maximum pilot lives
+csar.maxLives = 10 -- Maximum pilot lives
 
 csar.countCSARCrash = false -- If you set to true, pilot lives count for CSAR and CSAR aircraft will count.
 
