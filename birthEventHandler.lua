9514854d6221ecd4477c1f425133d17f76b25eac
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-01-10 12:35:25 +0000

    Restructure birthEventHandler

--- birthEventHandler.lua
+++ birthEventHandler.lua
@@ -11,23 +11,33 @@ M.BIRTH_EVENTHANDLER = {
 function M.BIRTH_EVENTHANDLER:New(restartHours)
     local _self = BASE:Inherit(self, EVENTHANDLER:New())
     _self.restartHours = restartHours
+    _self:HandleEvent(EVENTS.Birth, _self._OnBirth)
     return _self
 end
 
-function M.BIRTH_EVENTHANDLER:createMissionInfoMenu(event)
+function M.BIRTH_EVENTHANDLER:_OnBirth(event)
+    self:_AddMenus(event)
+end
+
+function M.BIRTH_EVENTHANDLER:_AddMenus(event)
     if event.IniPlayerName then
-        self:I("Adding mission info menu for " .. event.IniPlayerName)
         local playerGroup = event.IniGroup
-        MENU_GROUP_COMMAND:New(playerGroup, "Time until restart", nil, function()
-            local secondsUntilRestart = restartInfo.getSecondsUntilRestart(os.date("*t"), self.restartHours)
-            MESSAGE:New(string.format("The server will restart in %s", restartInfo.getSecondsAsString(secondsUntilRestart)), 5):ToGroup(playerGroup)
-        end)
+        if playerGroup then
+            self:I("Adding menus for " .. event.IniPlayerName)
+            self:_AddTimeUntilRestart(playerGroup)
+        end
     end
+end
+
+function M.BIRTH_EVENTHANDLER:_AddTimeUntilRestart(playerGroup)
+    MENU_GROUP_COMMAND:New(playerGroup, "Time until restart", nil, function()
+        local secondsUntilRestart = restartInfo.getSecondsUntilRestart(os.date("*t"), self.restartHours)
+        MESSAGE:New(string.format("The server will restart in %s", restartInfo.getSecondsAsString(secondsUntilRestart)), 5):ToGroup(playerGroup)
+    end)
 end
 
 function M.onMissionStart(restartHours)
     M.eventHandler = M.BIRTH_EVENTHANDLER:New(restartHours)
-    M.eventHandler:HandleEvent(EVENTS.Birth, M.eventHandler.createMissionInfoMenu)
 end
 
 return M
