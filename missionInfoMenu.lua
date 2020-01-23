3995060d8cfe2b3f5459b77604de9f1d1359b4f2
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-01-23 21:09:15 +0000

    Create "Mission info" menu group and move "Time until restart" under it

--- missionInfoMenu.lua
+++ missionInfoMenu.lua
@@ -3,7 +3,8 @@ local restartInfo = require("restartInfo")
 local M = {}
 
 function M.addMenu(playerGroup, restartHours)
-    MENU_GROUP_COMMAND:New(playerGroup, "Time until restart", nil, function()
+    local infoMenu = MENU_GROUP:New(playerGroup, "Mission info")
+    MENU_GROUP_COMMAND:New(playerGroup, "Time until restart", infoMenu, function()
         local secondsUntilRestart = restartInfo.getSecondsUntilRestart(os.date("*t"), restartHours)
         MESSAGE:New(string.format("The server will restart in %s", restartInfo.getSecondsAsString(secondsUntilRestart)), 5):ToGroup(playerGroup)
     end)
