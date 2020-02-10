e3f7ed1b99cef6e491900e4582b9fc85aa27e6dd
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-02-10 13:13:00 +0000

    Fix luacheck warnings in test_spatialUtils

--- tests/test_spatialUtils.lua
+++ tests/test_spatialUtils.lua
@@ -3,13 +3,13 @@ local spatialUtils = require("spatialUtils")
 
 TestSpatialUtils = {}
 
-local function timeFunction(iterationCount, fn)
-    local clockBefore = os.clock()
-    for _ = 1, iterationCount do
-        fn()
-    end
-    return os.clock() - clockBefore
-end
+--local function timeFunction(iterationCount, fn)
+--    local clockBefore = os.clock()
+--    for _ = 1, iterationCount do
+--        fn()
+--    end
+--    return os.clock() - clockBefore
+--end
 
 function TestSpatialUtils:testIsPointInZone()
     lu.assertIsTrue(spatialUtils.isPointInZone({ x = 0, y = 0 }, { x = 0, y = 0 }, 100))
@@ -25,20 +25,20 @@ function TestSpatialUtils:testFindNearest()
     lu.assertEquals(idx, 1)
     lu.assertAlmostEquals(minDist, 1.414, 0.001)
 
-    local idx, minDist = spatialUtils.findNearest({ x = 0.4, y = 0.4 }, points)
+    idx, minDist = spatialUtils.findNearest({ x = 0.4, y = 0.4 }, points)
     lu.assertEquals(idx, 1)
     lu.assertAlmostEquals(minDist, 0.566, 0.001)
 
     -- matches first found
-    local idx, minDist = spatialUtils.findNearest({ x = 0.5, y = 0.5 }, points)
+    idx, minDist = spatialUtils.findNearest({ x = 0.5, y = 0.5 }, points)
     lu.assertEquals(idx, 1)
     lu.assertAlmostEquals(minDist, 0.707, 0.001)
 
-    local idx, minDist = spatialUtils.findNearest({ x = 0.9, y = 0.9 }, points)
+    idx, minDist = spatialUtils.findNearest({ x = 0.9, y = 0.9 }, points)
     lu.assertEquals(idx, 2)
     lu.assertAlmostEquals(minDist, 0.1414, 0.001)
 
-    local idx, minDist = spatialUtils.findNearest({x=0, y=0}, {})
+    idx, minDist = spatialUtils.findNearest({x=0, y=0}, {})
     lu.assertIsNil(idx)
     lu.assertIsNil(minDist)
 end
