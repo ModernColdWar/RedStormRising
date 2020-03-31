5b5f279ac6db8b30c57351f94b4b284782bf0435
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-03-31 11:19:08 +0100

    Update with changes from server

--- Hooks/SimpleSlotBlockGameGUI.lua
+++ Hooks/SimpleSlotBlockGameGUI.lua
@@ -101,115 +101,123 @@ ssb.prefixes = {
 -- This script will output them when a player changes slots so you can copy them out easily :)
 -- This will only take effect if: ssb.controlNonAircraftSlots = true
 ssb.commanderPlayerUCID = {
-  "9afc4637083feaf599e78b25b37adf25",
-  "053ec863e71f9b83f1dc3155a3563ee6",
-  "f47be7b9564d03e6d8a75313efeba92a",
-  "64cb0b85fffe51c3e79ecd8f13766ded",
-  "0f8240487915af76a6f44a41c01cde47",
-  "182fb65bdbbdf9566d7cefeaab8c3c25",
-  "7d5c7a1b8bf4e5bee87cf9fd20d3a90c",
-  "e2b6cc3261d5513faaaffe8a7c157a32",
-  "a0d2bd3f8eea70563d49cae543f4ed8f",
-  "117bc9e9faafa19d71f099d650cf1b0c",
-  "ebb1471000e15b3ca240910d82342503",
-  "e711c21cecbfb91534afa41275027291",
-  "57c75549549a256464978b7a7deba4c8",
-  "9d70024558a2a3949c2ae9b8b9f79669",
-  "99bc992d15a8b9366c1af3873f53a895", --Capt.Fdez
-  "e5d0f5316232e5802ced7cc4c86d2c95",
-  "5d619981c0a192bda60c927bd6fbccf9",
-  "231aa8ac940aa8a712c1f17c2a3f0122",
-  "24b0e1cfc1b27e375d38a98a43db9779",
-  "cbb43fc99e978ee535b86653d14e5dbc",
-  "d7f6161e6401a1c9c1f31ba7315e647c",
-  "60eaa32a48cdf3b1162739acf7cfec28", --DakiBajoMoj
-  "6e1c8117ab788fa693feec6326320a5e",
-  "842514dc165092ba9631f8c7d2052d0a",
-  "837f1acd1132ed0a60f2a6a1f57bcc1c", --Smitty
-  "263d36e3b60782a6c16e2b3c5cebf3f9",
-  "b12d9af45f2b08a353f1e706e034eaec",
-  "0a312f0e5531d0fb0e1b1d90bb559fa3",
-  "e011e49e75560dbe802d50cd0f1ce68d",
-  "80981e82a3f8b8178c232ddc96d7fe40",
-  "3beb7082568d91bdeb29282d840c4e76",
-  "3b1623a591687015f620aaba1598b3ca",
-  "6151a2842ea86fd5bf7274fdf0cce579",
-  "67d27f9376433f555b1ae71c2872c584",
-  "e5f8848b3ef84a88c635e5bc2a8e2a41",
-  "f3120f75fa5d66e5a1adaeea4f303c3d",
-  "a9a6ba83cc166d960fcf7ef3d8a20eb9",
-  "c0fda776e215e597c37c92fb9dde70fb",
-  "9d76164a91c872953a4f1ab378f0e008",
-  "a78d9cc5ad679f472dc36f66c71d99db", --Canuck
-  "dc91af6f5f48dd94c89ace508c17cc69",
-  "eae6f83ee3693287e92229d7eeab25b3",
-  "5df2d8f9d4675adf07a1d0f7eed7f9e0",
-  "b148135d459915551e3d2b5489821c16",
-  "00e5296e187ccfb8ee5edb55a7968d39", --★ВАРЯГ★
-  "25bd8aec82ad88e26c5534abddb3cc22", --HanSolo
-  "b0d7c14dd6fe1ac1ef203643512f4387", --Two Lives
-  "aea4fb0972eb001320d390b7a955356b", --НовоБранец 
-  "1d086e78fb0870ad682ff5ee90aa94fd", ---=WildCat=-
-  "22f8d3edba38b8052c3b82242262a867", --Shatten55 
-  "a1aef3ef90d4e611eb8bd40a21ab26cd", --Coldays
-  "46f14d9df21bcca261da5d54ff77b6a8", --Winston
-  "630959d9628dce969646b5ce279a5996", --crazzymrbc
-  "90d0af5d35b62cc5012de966b5bd1dcf", --Harkonnen
-  "6e14d6e4deb4bf3b08d99713754cd1dc", --n0xy Steam
-  "3dd6e3bcd9f99a8826e8f9791dc15199", --Sakhalin66
-  "4052f4b970efd04829623d087898a66f", --=KAG=Zakha
-  "53c1017106f484956c9e56d8c4b85acb", --GenaPilot
-  "18228f3b7ecb8b5af9cb7965870d3f1b", --EARL 4-2 | H4nS0L0
-  "822f0042190141165d100ccffe8d2cbf", --EARL 1-4 | alxz
-  "fff7b6fabadc7339a246fa5330ceb3b2", --SaabFAN
-  "a1f7bf58006730415c9cdda22b2f89b2", --EARL 6-4 | CYPHER
-  "5d194005968acd3db845fdae1f1575e9", --Checklist
-  "07aa971b0e583a05c69dc06075bedabf", --HansPeter
-  "703a4d348871ec2fe9cac9cd2168f609", --NightStalker
-  "5cb74e04677581b97c65d7ac67872b99", --Woodstock
-  "a73cc77ec3926ac636a4fd419f1c0fb5", --8==D Jarhead
-  "c7dade15a8a58dca9d91e12402b77e65", --8==D Jarhead
-  "b7477d13cfac623b3ca55e1e2dd0df1f", --8==D Guns
-  "e64dcbab81dcdb157439018fc6e76488", --8==D Zooker
-  "e1277ddbc6cc620bfbc9ceb0f57dc19c", --Airplaneman321
-  "2e9713fc055304fe1bd6c6c778c0f620", --LeoNerd
-  "91ec71924c23f4f15d785f5d7105e6b3", --Salad
-  "87dd983a6c5b678de30c9adb55b18cf4", --n0xy Standalone
-  "9b1a9d6714bfc414eb0c2e5158f75c89", ---=*26th*=- | Pakster
-  "4f306d55e7ca9d3c683ebd983ad6c859", --Firefly
-  "afce946e4b300ebc03bbbfdee7b07f31", --<64>Kazansky
-  "10fb130391f045766bb836e7c9f66c2c", --Viper 77|Gandalf
-  "964158664f15dd3d7fbb0ea80c6b296f", --The State|LTJG SNOT
-  "20dd4b54a9fc51df4d38cd8955103361", --Disco
-  "4c38b2e3c3647473512e5400017df50e", --8==D Ritt
-  "92fd1d66e41dfa65c7dd73b4f309db87", --8==D Texas
-  "f10c00accc6b3f694490dbb307988e1d", --ROGUE | Archangel
-  "6cb230729c620cd22eea9a1900279dcb", --GasMachine
-  "2afada4c6dcd47147a1140cea34bc43a", --8==D Redback
-  "d64300a70517b5878f2aae5cc4e96968", --8==D Shively
-  "a72632f9d4aef4bc3fc12fb0a6bcdc87", --=R|D= johnnytrump
-  "d125549aa96ca9c8c021e7bfb1028d7c", --Tester
-  "d26bb6e1f0b5268626d41a0feef17727", --Shabi
-  "b67841fbaf484d62fd93c159f00a5a78", --ALA15-Snake
-  "19db9ec1f7b12df8c89360e17a31d7ef", --VS33 Stroke
-  "53ecb670a7fa0147ccf6ab8af0eb7f9d", --wolfy
-  "1d62619fb7ccd513e9f7cd639ad595bb", --Lam, added by Mobius
-  "0905260d71275b3f8051e42d73b96a19", --{WD} bert, added by Capt.Fdez
-  "7e8563b5c0ae6d9657de5758395c2406", -- ZZ added by deebix
-  "079236f5f48b0350bdaf364d042dccfe", -- rocket_knight added by deebix
-  "09bc71361a591195159e2b84dfc67482", --ROSS Pups added by deebix
-  "8f3e2a5beadbd739a973027607f4a5cc", --Rigez added by mad rabbit
-  "7c6028ee85d15cb5008a07403f486522", --outcast 1-1 by deebix
-  "37922d84eaf6bb71e775a54af9b1c75d", --Drip added by deebix
-  "88e28730b17a4cacae63e61fafbded1e", -- Airogue added by mad rabbit
-  "a83dd704a242de5450deec9887e7c9d3", -- Sephryn added by mad rabbit
+	----------------------------------
+	-- RSR DEV
+	"46f14d9df21bcca261da5d54ff77b6a8", -- Winston
+	"117bc9e9faafa19d71f099d650cf1b0c", -- deebix
+	"cbb43fc99e978ee535b86653d14e5dbc", -- Mobius
+	"99bc992d15a8b9366c1af3873f53a895", -- Capt.Fdez
+	"231aa8ac940aa8a712c1f17c2a3f0122", -- Flashheart
+	"6e14d6e4deb4bf3b08d99713754cd1dc", -- n0xy Steam
+	"87dd983a6c5b678de30c9adb55b18cf4", -- n0xy Standalone
+	"263d36e3b60782a6c16e2b3c5cebf3f9", -- mad rabbit
+	----------------------------------
+	"9afc4637083feaf599e78b25b37adf25",
+	"053ec863e71f9b83f1dc3155a3563ee6",
+	"f47be7b9564d03e6d8a75313efeba92a",
+	"64cb0b85fffe51c3e79ecd8f13766ded", -- Wildcat
+	"0f8240487915af76a6f44a41c01cde47",
+	"182fb65bdbbdf9566d7cefeaab8c3c25",
+	"7d5c7a1b8bf4e5bee87cf9fd20d3a90c", -- Lord Felix
+	"e2b6cc3261d5513faaaffe8a7c157a32",
+	"a0d2bd3f8eea70563d49cae543f4ed8f",
+	"ebb1471000e15b3ca240910d82342503",
+	"e711c21cecbfb91534afa41275027291", -- Hbomb
+	"57c75549549a256464978b7a7deba4c8",
+	"9d70024558a2a3949c2ae9b8b9f79669",
+	"e5d0f5316232e5802ced7cc4c86d2c95",
+	"5d619981c0a192bda60c927bd6fbccf9",
+	"24b0e1cfc1b27e375d38a98a43db9779",
+	"d7f6161e6401a1c9c1f31ba7315e647c",
+	"60eaa32a48cdf3b1162739acf7cfec28", --DakiBajoMoj
+	"6e1c8117ab788fa693feec6326320a5e",
+	"842514dc165092ba9631f8c7d2052d0a",
+	"837f1acd1132ed0a60f2a6a1f57bcc1c", -- Smitty
+	"b12d9af45f2b08a353f1e706e034eaec",
+	"0a312f0e5531d0fb0e1b1d90bb559fa3",
+	"e011e49e75560dbe802d50cd0f1ce68d",
+	"80981e82a3f8b8178c232ddc96d7fe40",
+	"3beb7082568d91bdeb29282d840c4e76",
+	"3b1623a591687015f620aaba1598b3ca",
+	"6151a2842ea86fd5bf7274fdf0cce579",
+	"67d27f9376433f555b1ae71c2872c584",
+	"f3120f75fa5d66e5a1adaeea4f303c3d", -- Drif
+	"a9a6ba83cc166d960fcf7ef3d8a20eb9",
+	"c0fda776e215e597c37c92fb9dde70fb",
+	"9d76164a91c872953a4f1ab378f0e008",
+	"a78d9cc5ad679f472dc36f66c71d99db", -- Canuck
+	"dc91af6f5f48dd94c89ace508c17cc69",
+	"eae6f83ee3693287e92229d7eeab25b3",
+	"5df2d8f9d4675adf07a1d0f7eed7f9e0",
+	"b0d7c14dd6fe1ac1ef203643512f4387", -- Two Lives
+	"22f8d3edba38b8052c3b82242262a867", -- Shatten55 
+	"630959d9628dce969646b5ce279a5996", -- crazzymrbc
+	"18228f3b7ecb8b5af9cb7965870d3f1b", -- EARL 4-2 | H4nS0L0
+	"822f0042190141165d100ccffe8d2cbf", -- EARL 1-4 | alxz
+	"fff7b6fabadc7339a246fa5330ceb3b2", -- SaabFAN
+	"a1f7bf58006730415c9cdda22b2f89b2", -- EARL 6-4 | CYPHER
+	"5d194005968acd3db845fdae1f1575e9", -- Checklist
+	"07aa971b0e583a05c69dc06075bedabf", -- HansPeter
+	"703a4d348871ec2fe9cac9cd2168f609", -- NightStalker
+	"5cb74e04677581b97c65d7ac67872b99", -- Woodstock
+	"e1277ddbc6cc620bfbc9ceb0f57dc19c", -- Airplaneman321
+	"2e9713fc055304fe1bd6c6c778c0f620", -- LeoNerd
+	"91ec71924c23f4f15d785f5d7105e6b3", -- Salad
+	"9b1a9d6714bfc414eb0c2e5158f75c89", -- -=*26th*=- | Pakster
+	"4f306d55e7ca9d3c683ebd983ad6c859", -- Firefly
+	"afce946e4b300ebc03bbbfdee7b07f31", -- <64>Kazansky
+	"10fb130391f045766bb836e7c9f66c2c", -- Viper 77|Gandalf
+	"964158664f15dd3d7fbb0ea80c6b296f", -- The State|LTJG SNOT
+	"20dd4b54a9fc51df4d38cd8955103361", -- Disco
+	"4c38b2e3c3647473512e5400017df50e", -- 8==D Ritt
+	"92fd1d66e41dfa65c7dd73b4f309db87", -- 8==D Texas
+	"2afada4c6dcd47147a1140cea34bc43a", -- 8==D Redback
+	"d64300a70517b5878f2aae5cc4e96968", -- 8==D Shively
+	"a73cc77ec3926ac636a4fd419f1c0fb5", -- 8==D Jarhead
+	"c7dade15a8a58dca9d91e12402b77e65", -- 8==D Jarhead
+	"b7477d13cfac623b3ca55e1e2dd0df1f", -- 8==D Guns
+	"e64dcbab81dcdb157439018fc6e76488", -- 8==D Zooker
+	"f10c00accc6b3f694490dbb307988e1d", -- ROGUE | Archangel
+	"6cb230729c620cd22eea9a1900279dcb", -- GasMachine
+	"a72632f9d4aef4bc3fc12fb0a6bcdc87", -- =R|D= johnnytrump
+	"d125549aa96ca9c8c021e7bfb1028d7c", -- Tester
+	"d26bb6e1f0b5268626d41a0feef17727", -- Shabi
+	"b67841fbaf484d62fd93c159f00a5a78", -- ALA15-Snake
+	"19db9ec1f7b12df8c89360e17a31d7ef", -- VS33 Stroke
+	"53ecb670a7fa0147ccf6ab8af0eb7f9d", -- wolfy
+	"0905260d71275b3f8051e42d73b96a19", -- {WD} bert, added by Capt.Fdez
+	"7e8563b5c0ae6d9657de5758395c2406", -- ZZ added by deebix
+	"8f3e2a5beadbd739a973027607f4a5cc", -- Rigez added by mad rabbit
+	"7c6028ee85d15cb5008a07403f486522", -- outcast 1-1 by deebix
+	"37922d84eaf6bb71e775a54af9b1c75d", -- Drip added by deebix
+	"88e28730b17a4cacae63e61fafbded1e", -- Airogue added by mad rabbit
+	"a83dd704a242de5450deec9887e7c9d3", -- Sephryn added by mad rabbit
+	"079236f5f48b0350bdaf364d042dccfe", -- rocket_knight added by deebix
+	"c70c2a7bb2d855e80bcf4964ee1daaeb", -- moupe added by deebix
+	"9de0e2e317a56c09aecff8eeeecdd47a", -- =AW=33COM added by mad rabbit
+	-----------------------------------
+	-- Russian Players
+	"53c1017106f484956c9e56d8c4b85acb", -- GenaPilot
+	"09bc71361a591195159e2b84dfc67482", -- ROSS_Pups added by deebix
+	"20ef9ad2392e236cf72cd3f3a853da42", -- ROSS_BoomSbk added by mad rabbit
+	"3dd6e3bcd9f99a8826e8f9791dc15199", -- Sakhalin66
+	"90d0af5d35b62cc5012de966b5bd1dcf", -- Harkonnen
+	-----------------------------------
+	-- Russian Players
+	"00e5296e187ccfb8ee5edb55a7968d39", -- ★ВАРЯГ★
+	"b148135d459915551e3d2b5489821c16", -- Santa
+	"e5f8848b3ef84a88c635e5bc2a8e2a41", -- Djim
+	"25bd8aec82ad88e26c5534abddb3cc22", -- HanSolo
+	"aea4fb0972eb001320d390b7a955356b", -- НовоБранец 
+	"a1aef3ef90d4e611eb8bd40a21ab26cd", -- Coldays
+	"1d62619fb7ccd513e9f7cd639ad595bb", -- Lam, added by Mobius
+	"4052f4b970efd04829623d087898a66f", -- =KAG=Zakha
+	"1d086e78fb0870ad682ff5ee90aa94fd", -- -=WildCat=-
+	-----------------------------------
+
 }
 
-
-
 ssb.version = "1.1"
-
-
 
 -- Logic for determining if player is allowed in a slot
 function ssb.shouldAllowAircraftSlot(_playerID, _slotID) -- _slotID == Unit ID unless its multi aircraft in which case slotID is unitId_seatID
