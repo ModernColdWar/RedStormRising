ddec594ffddfc3f0763a0d8559e51e7b2b66ead7
Winston2108 <david.dunwoody+winston2108@gmail.com>
2020-04-09 15:41:26 +0100

    Allow banning of players by IP or UCID

--- Hooks/RSRPlayerLogger.lua
+++ Hooks/RSRPlayerLogger.lua
@@ -5,6 +5,16 @@ local lfs = require('lfs')
 
 local M = {}
 
+-- Banned IP addresses
+M.bannedIps = {
+
+}
+
+-- Banned player UCIDs
+M.bannedUcids = {
+    "7c0dc87190061fd98dcf2936444ba05c", -- "nuke china!!!" 2020-04-09
+}
+
 M.db = sqlite3.open(lfs.writedir() .. [[Scripts\RSR\rsr.sqlite]], sqlite3.OPEN_READWRITE)
 
 M.insert_stmt = M.db:prepare("INSERT INTO connection(ucid, name, ipaddr) VALUES(?, ?, ?)")
@@ -17,6 +27,20 @@ function M.onPlayerConnect(playerId)
     M.insert_stmt:bind_values(ucid, name, ipaddr)
     M.insert_stmt:step()
     M.insert_stmt:reset()
+end
+
+-- based on slmod from https://github.com/mrSkortch/DCS-SLmod/blob/master/Scripts/net/Slmodv7_5/SlmodCallbacks.lua
+function M.onPlayerTryConnect(ipaddr, name, ucid)
+    if M.bannedIps[ipaddr] then
+        net.log("RSRPlayerLogger: not allowing " .. name .. ", ucid " .. ucid .. " from banned IP" .. ipaddr)
+        return false, "You are banned from this server."
+    end
+
+    if M.bannedUcids[ucid] then
+        net.log("RSRPlayerLogger: not allowing " .. name .. ", ucid " .. ucid .. " from banned ucid" .. ucid)
+        return false, "You are banned from this server."
+    end
+    -- return nothing
 end
 
 DCS.setUserCallbacks(M)
